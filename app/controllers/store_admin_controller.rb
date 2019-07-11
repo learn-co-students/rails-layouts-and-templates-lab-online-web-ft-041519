f03b495709a60e062e76c895e374af2964429942
Kish-Siva-1 <rodes88@msn.com>
2019-07-11 23:50:32 +0200

    Done.

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -1,3 +1,15 @@
-class StoreAdminController
+class StoreAdminController < ApplicationController
     
+    def home
+        render :layout => "admin"
+    end 
+
+    def orders
+        render :layout => "order_administration"    
+    end 
+
+    def invoice 
+        render :layout => false 
+    end 
+
 end 
